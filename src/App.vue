932eae131e9b447608f1af5cda58f457d5f22806
Siva Prakash <prakashdeveloper@outlook.in>
2023-08-13 10:51:47 +0530

    Initial commit & Added tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import movies from './movies.json'
+
+import Magnify from 'vue-material-design-icons/Magnify.vue'
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
+import TrendingUp from 'vue-material-design-icons/TrendingUp.vue'
+import Television from 'vue-material-design-icons/Television.vue'
+import MovieOutline from 'vue-material-design-icons/MovieOutline.vue'
+import Plus from 'vue-material-design-icons/Plus.vue'
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
+
+import { useMovieStore } from './stores/movie'
+import { storeToRefs } from 'pinia'
+const useMovie = useMovieStore()
+const { movie, showFullVideo } = storeToRefs(useMovie)
+
+let video = ref(null)
+
+onMounted(() => {
+  setTimeout(() => (movie.value = movies[0][0]), 100)
+})
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
